95b778a2826c137512ac0fd24bf6473ff59ac0c1
Mai <mai.ho25@student.passerellesnumeriques.org>
2024-06-14 15:56:02 +0700

    divice component header

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
+import { useState } from "react";
 import pnLogo from "./assets/pn-logo.png";
-
+import Header from "./components/Header";
+import Score from "./components/Score";
 function App() {
+  const [batchName, setBatchName] = useState("");
   return (
     <>
-      <header id="header">
-        <img src={pnLogo} alt="PN Logo" />
-        <h1>Students results for (fake batch name))</h1>
-      </header>
-
+      <Header batchname = {batchName} />
       <main className="scores-container">
-        <div class="scores">
-          <h1>Fake Course</h1>
-
-          <table>
-            <thead>
-              <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>First name 1 </td>
-                <td>Last name 1 </td>
-                <td>55</td>
-              </tr>
-              <tr>
-                <td>First name 2 </td>
-                <td>Last name 2 </td>
-                <td>45</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <Score courseName={batchName} listResult = {batchName}/>
       </main>
     </>
   );
